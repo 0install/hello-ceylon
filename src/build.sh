83bc1804575922510f0b01ca9a6f68a69187c07a
Thomas Leonard <talex5@gmail.com>
2011-11-26 16:44:17 +0000

    Better idea: just depend on 0compile 0.25 :-)

--- src/build.sh
+++ src/build.sh
@@ -4,12 +4,8 @@ if [ "$SRCDIR" = "" ]; then
 	exit 1
 fi
 
-# This is cleaner, but requires 0install >= 1.4
-#"$CEYLONC" -src "$SRCDIR/src" -out "$BUILDDIR" "$SRCDIR/src/helloworld.ceylon"
-
-# Ugly way that works with older versions:
-"$JRE_BIN"java -enableassertions -Xbootclasspath/p:$JAVA_CP com.redhat.ceylon.compiler.Main -classpath $COMPILE_CP \
-    -src "$SRCDIR/src" -out "$BUILDDIR" "$SRCDIR/src/helloworld.ceylon"
+# Compile
+"$CEYLONC" -src "$SRCDIR/src" -out "$BUILDDIR" "$SRCDIR/src/helloworld.ceylon"
 
 # Copy result to DISTDIR
 cp "$BUILDDIR/unversioned/default_module-unversioned.car" "$DISTDIR/helloworld.car"
